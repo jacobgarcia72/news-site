222c178d84c5b668873ef82d3524f868299528b8
Jacob Garcia <yockub@gmail.com>
2026-05-01 21:52:18 -0400

    refactor to separate news filter components

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -2,61 +2,76 @@ import NewsList from "@/components/news-list";
 import { getAbbreviatedMonth } from "@/lib/format";
 import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import Link from "next/link";
+import { Suspense } from "react";
+
+function FilterHeader({ year, month }) {
+    const availableYears = getAvailableNewsYears();
+    let links = availableYears;
+
+    if (
+        year && !availableYears.includes(year) ||
+        month && !getAvailableNewsMonths(year).includes(month)
+    ) {
+        throw new Error('Invalid filter.')
+    }
+
+    if (year && !month) {
+        links = getAvailableNewsMonths(year);
+    } else if (year && month) {
+        links = [];
+    }
+    return (
+        <header id="archive-header">
+            <nav>
+                <ul>
+                    {links.map((link) => {
+                        let href = `/archive/${link}`;
+                        let linkDisplayText = link;
+                        if (year) {
+                            href = `/archive/${year}/${link}`;
+                            linkDisplayText = getAbbreviatedMonth(Number(link));
+                        }
+                        return (
+                            <li key={link}>
+                                <Link href={href}>
+                                    {linkDisplayText}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+function FilteredNews({ year, month }) {
+    let news;
+    if (year && !month) {
+        news = getNewsForYear(year);
+    } else if (year && month) {
+        news = getNewsForYearAndMonth(year, month);
+    }
+    if (news && news.length) {
+        return <NewsList articles={news} />
+    } else {
+        return <p>No news found for the selected time period.</p>
+    }
+}
 
 export default function FilteredNewsPage({ params }) {
     const filter = params.filter;
     const selectedYear = filter?.[0];
     const selectedMonth= filter?.[1];
-
-    let news; 
-    let links = getAvailableNewsYears();
-
-    if (selectedYear && !selectedMonth) {
-        news = getNewsForYear(selectedYear);
-        links = getAvailableNewsMonths(selectedYear);
-    } else if (selectedYear && selectedMonth) {
-        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
-        links = [];
-    }
-
-    let newsContent = <p>No news found for the selected time period.</p>
-
-    if (news && news.length) {
-        newsContent = <NewsList articles={news} />
-    }
-
-    if (
-        selectedYear && !getAvailableNewsYears().includes(selectedYear) ||
-        selectedMonth && !getAvailableNewsMonths(selectedYear).includes(selectedMonth)
-    ) {
-        throw new Error('Invalid filter.')
-    }
-
     return (
         <>
-            <header id="archive-header">
-                <nav>
-                    <ul>
-                        {links.map((link) => {
-                            let href = `/archive/${link}`;
-                            let linkDisplayText = link;
-                            if (selectedYear) {
-                                href = `/archive/${selectedYear}/${link}`;
-                                linkDisplayText = getAbbreviatedMonth(Number(link));
-                            }
-
-                            return (
-                                <li key={link}>
-                                    <Link href={href}>
-                                        {linkDisplayText}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-            </header>
-            {newsContent}
+            <Suspense fallback={<p>Loading filter options...</p>}>
+                <FilterHeader year={selectedYear} month={selectedMonth} />
+            </Suspense>
+            <Suspense fallback={<p>Loading news...</p>}>
+                <FilteredNews year={selectedYear} month={selectedMonth} />
+            </Suspense>
         </>
     )
 }
